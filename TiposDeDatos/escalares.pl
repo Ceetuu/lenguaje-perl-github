e388dde7486da43b6479c9e527304bf75a551416
Ceetu <carlitos_c_1996@hotmail.com>
2016-03-15 18:57:33 -0600

    Create escalares.pl

---
+++ TiposDeDatos/escalares.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $a = 12;
+print "\nTu variable tiene un valor de: $a.\nEl cuadrado es: ",$a*$a,"\n";
+
+my $b =3.5; #Para todos los datos escalares solamente basta con poner '$', el mismo lenguaje no te pide que marques la diferencia entre si es un dato tipo doble, entero o caracter.
+print "b: $b\n";
+
+my $word = "carlos";
+print "Tu palabra: $word\n";
+
+print "Tu programa terminó.\n";#El programa si puede ejecutar y mostrar en terminal las comas.
