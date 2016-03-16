286e22d2444ad4a465b7d5b5a50350c815943303
Ceetu <carlitos_c_1996@hotmail.com>
2016-03-15 18:56:42 -0600

    Added files via upload

---
+++ holaPerl.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Hi Ceetu! Nice to meet you!\n";
+print "42\n";
+my $num = 13;
+print "$num\n";
+print '$num\n'; #Se imprime literlamente lo que hay dentro de las comillas simples.
+
+#El código va en secuencia linea por linea de arriba hacia abajo, sin función main.
+
+#Comentarios
+
+
+print "\n";
